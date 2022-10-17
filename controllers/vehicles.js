492ddf646d6ac3151dd3732002569d7789279f83
aibrahim@hsk-partners.com <ahmadji.ibr11@gmail.com>
2022-10-18 01:05:35 +0200

    create vehicle function with its route

--- controllers/vehicles.js
+++ controllers/vehicles.js
@@ -15,16 +15,43 @@ const getVehicles = ((req, res) => {
  * @type {(function(*, *): (*|undefined))|*}
  */
 const getVehicle = ((req, res) => {
+    //get the id of the vehicle that we want to find
     const id = Number(req.params.id)
+    //search for the vehicle
     const vehicle = vehicles.find(vehicle => vehicle.id === id)
 
+    //check if the vehicle found or nah
     if (!vehicle) {
         return res.status(404).send('Vehicle not found')
     }
     res.json(vehicle)
 })
 
+/**
+ * Create a new object of vehicle
+ *
+ * @type {createVehicle}
+ */
+const createVehicle = ((req, res) => {
+
+    //create the vehicle array from the data sent from user
+    const newVehicle = {
+        id    : vehicles.length + 1,
+        type  : req.body.type,
+        brand : req.body.brand,
+        model : req.body.model,
+        color : req.body.color,
+        year  : req.body.year
+    }
+
+    //put the array created above in the array vehicles that contains all vehicles
+    vehicles.push(newVehicle);
+
+    res.status(201).json(newVehicle);
+})
+
 export {
     getVehicles,
-    getVehicle
+    getVehicle,
+    createVehicle,
 }
--- routes/vehicles.js
+++ routes/vehicles.js
@@ -1,12 +1,12 @@
 import express from "express";
 import {
-    getVehicles,
-    getVehicle
+    getVehicles, getVehicle, createVehicle
 } from '../controllers/vehicles.js'
 
 const vehiclesRouter = express.Router()
 
 vehiclesRouter.get("/", getVehicles)
 vehiclesRouter.get("/:id", getVehicle)
+vehiclesRouter.post("/create", createVehicle)
 
 export {vehiclesRouter}
